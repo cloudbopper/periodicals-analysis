24a4b5d8c2c3696194901ef9e42bd05f0db95438
Akshay Sood <sood@cs.wisc.edu>
2018-06-05 16:31:04 -0500

    Use pandas

--- perysis/load_documents.py
+++ perysis/load_documents.py
@@ -2,35 +2,20 @@
 Loads documents for topic modeling
 """
 
-import csv
+from pandas import read_csv
 
 METADATA_FILENAME = "Publication_Timestamps.csv"
 PUBLICATION = "Publication"
 FILENAME = "File Name"
 YEAR = "Year"
-
-class Document:
-    """Document data class"""
-    # pylint: disable = too-few-public-methods
-    name: str
-    data: str
-    publication: str
-    year: int
-
-    def __init__(self, name, publication, year):
-        self.name = name
-        self.publication = publication
-        self.year = year
-
+DATA = "Data"
 
 def load_documents(input_dir):
     """Loads document data and meta-data"""
-    documents = []
-    with open("%s/%s" % (input_dir, METADATA_FILENAME)) as metadata_file:
-        reader = csv.DictReader(metadata_file)
-        for row in reader:
-            document = Document(row[FILENAME], row[PUBLICATION], int(row[YEAR]))
-            with open("%s/%s.txt" % (input_dir, document.name)) as data_file:
-                document.data = data_file.read()
-            documents.append(document)
+    documents = read_csv("%s/%s" % (input_dir, METADATA_FILENAME))
+    data = []
+    for filename in documents[FILENAME]:
+        with open("%s/%s.txt" % (input_dir, filename)) as data_file:
+            data.append(data_file.read())
+    documents[DATA] = data
     return documents
